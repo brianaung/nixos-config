c9957e88d028462029012cc6bb42b070d96c8224
Brian Aung <brian@boutique.tech>
2023-10-24 18:27:23 +1100

    style: add borders to lsp window

--- dotfiles/nvim/lua/plugins/lsp.lua
+++ dotfiles/nvim/lua/plugins/lsp.lua
@@ -3,6 +3,24 @@ local nvim_lsp = require "lspconfig"
 -- local on_attach = function(client, bufnr)
 -- end
 
+local _border = "rounded"
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover, {
+    border = _border
+  }
+)
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+  vim.lsp.handlers.signature_help, {
+    border = _border
+  }
+)
+
+vim.diagnostic.config{
+  float={border=_border}
+}
+
 -- Enable following language servers
 local servers = {
   tsserver = {},
