2e536f556a7ea58a67e71e0b5c8cbf88bea0bb6a
Brian Aung <brianaung16@gmail.com>
2023-12-02 22:30:25 +1100

    update some options

--- dotfiles/nvim/plugin/options.lua
+++ dotfiles/nvim/plugin/options.lua
@@ -5,9 +5,9 @@ o.timeoutlen = 500
 o.number = true
 o.relativenumber = true
 
-o.tabstop = 2
-o.shiftwidth = 2
-o.softtabstop = 2
+o.tabstop = 4
+o.shiftwidth = 4
+o.softtabstop = 4
 o.expandtab = true
 
 o.ignorecase = true
@@ -17,8 +17,21 @@ o.breakindent = true
 o.showbreak = "   "
 o.linebreak = true
 
+-- i trust tj
 o.formatoptions = o.formatoptions
+  - "a" -- Auto formatting is BAD.
+  - "t" -- Don't auto format my code. I got linters for that.
+  + "c" -- In general, I like it when comments respect textwidth
+  + "q" -- Allow formatting comments w/ gq
   - "o" -- O and o, don't continue comments
   + "r" -- But do continue when pressing enter.
+  + "n" -- Indent past the formatlistpat, not underneath it.
+  + "j" -- Auto-remove comments if possible.
+  - "2" -- I'm not in gradeschool anymore
 
 vim.cmd("au BufEnter * set fo-=o")
+
+-- o.list = true
+vim.cmd(
+  [[set listchars=tab:→\ ,eol:↲,extends:›,precedes:‹,nbsp:␣,trail:~]]
+)
