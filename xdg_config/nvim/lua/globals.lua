abe310274f8ce1fa9d4cc08f83922114fe262f15
Brian Aung <brianaung16@gmail.com>
2024-01-02 01:51:20 +1100

    refactor neovim config

--- xdg_config/nvim/lua/globals.lua
+++
@@ -1,6 +0,0 @@
--- globals that i always expect to be available
-
-P = function(thing)
-  print(vim.print(thing))
-  return thing
-end
--- xdg_config/nvim/lua/snippets/lua.lua
+++
@@ -1,9 +0,0 @@
-local ls = require("luasnip")
-local s = ls.snippet
-local i = ls.insert_node
-local fmt = require("luasnip.extras.fmt").fmt
-
-ls.add_snippets("lua", {
-  s("fn", fmt("function {}({})\n\t{}\nend", { i(1), i(2), i(3) })),
-  s("req", fmt('local {} = require("{}")', { i(1), i(2) })),
-})
