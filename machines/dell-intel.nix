c5af0eec684999453d52b9a09221551d4a507f4f
Brian Aung <brianaung16@gmail.com>
2024-03-11 21:50:21 +1100

    setup gpu drivers

--- machines/dell-intel.nix
+++ machines/dell-intel.nix
@@ -1,21 +1,15 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { config, lib, pkgs, ... }:
 
 {
   imports =
-	[ # Include the results of the hardware scan.
+	[
 	  ./hardwares/dell-intel.nix
 	  ./nixos-shared.nix
 	];
 
-	# Use the systemd-boot EFI boot loader.
 	boot.loader.systemd-boot.enable = true;
 	boot.loader.efi.canTouchEfiVariables = true;
 
-	# Enable sound with pipewire.
 	sound.enable = true;
 	hardware.pulseaudio.enable = false;
 	security.rtkit.enable = true;
@@ -24,30 +18,29 @@
 		alsa.enable = true;
 		alsa.support32Bit = true;
 		pulse.enable = true;
-		# If you want to use JACK applications, uncomment this
-		#jack.enable = true;
-
-		# use the example session manager (no others are packaged yet so this is enabled by default,
-		# no need to redefine it in your config for now)
-		#media-session.enable = true;
 	};
 
-	# This option defines the first version of NixOS you have installed on this particular machine,
-	# and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-	#
-	# Most users should NEVER change this value after the initial install, for any reason,
-	# even if you've upgraded your system to a new NixOS release.
-	#
-	# This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-	# so changing it will NOT upgrade your system.
-	#
-	# This value being lower than the current NixOS release does NOT mean your system is
-	# out of date, out of support, or vulnerable.
-	#
-	# Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-	# and migrated your data accordingly.
-	#
-	# For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+	# Enable OpenGL
+	hardware.opengl = {
+		enable = true;
+		driSupport = true;
+		driSupport32Bit = true;
+	};
+
+	services.xserver.videoDrivers = ["nvidia"];
+
+	hardware.nvidia = {
+		package = config.boot.kernelPackages.nvidiaPackages.stable;
+		modesetting.enable = true; # required
+		open = false;
+		nvidiaSettings = true;
+		prime = {
+			sync.enable = true;
+			intelBusId = "PCI:0:2:0";
+			nvidiaBusId = "PCI:1:0:0";
+		};
+	};
+
 	system.stateVersion = "23.11"; # Did you read the comment?
 }
 
