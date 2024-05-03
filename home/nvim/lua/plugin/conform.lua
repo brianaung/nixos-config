29f8cf866b38dd4e5f4573d433749c34c24bfdff
brianaung <brianaung16@gmail.com>
2024-05-03 21:45:47 +1000

    rustfmt

--- home/nvim/lua/plugin/conform.lua
+++ home/nvim/lua/plugin/conform.lua
@@ -4,6 +4,7 @@ return {
 		formatters_by_ft = {
 			lua = { 'stylua' },
 			go = { 'gofmt' },
+			rust = { 'rustfmt' },
 			python = { 'isort', 'black' },
 			javascript = { { 'prettierd', 'prettier' } },
 			typescript = { { 'prettierd', 'prettier' } },
