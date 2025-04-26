d9e3f706b7a6f044ddf80dc52184f92affb1fab9
brianaung <brianaung16@gmail.com>
2025-04-27 00:56:47 +1000

    remove steam

--- hosts/thorin/configuration.nix
+++ hosts/thorin/configuration.nix
@@ -8,14 +8,8 @@
   networking.hostName = "thorin"; # Define your hostname.
 
   users.users.${user} = {
-    extraGroups = [ "gamemode" ];
     packages = with pkgs; [
       obs-studio
-      discord
     ];
   };
-
-  programs.steam.enable = true;
-  programs.steam.gamescopeSession.enable = true;
-  programs.gamemode.enable = true;
 }
